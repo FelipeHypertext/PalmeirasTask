98c3849ee4ee537f44a84f36625b939020672430
João Felipe <joaofe.mokdse@gmail.com>
2026-06-11 17:52:00 -0300

    Add PHP file header with team member details

--- palmeiras/index.php
+++ palmeiras/index.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * TRABALHO PHP — Prof. João Paulo Nunes da Silva
+ * 1. João Felipe Mokdse Costa           - RGM: 43492801
+ * 2. Victor Schernikau B. B. Vieira     - RGM: 44050496
+ * 3. Max Lopes                          - RGM: 42826381
+ * 4. Israel Wendell Cardoso Costa       - RGM: 43905412
+ * 5. Eduarda Luiz                       - RGM: 43806414
+ * 6. Douglas Menegotti                  - RGM: 37274261
+ */
 declare(strict_types=1);
 namespace App;
 
